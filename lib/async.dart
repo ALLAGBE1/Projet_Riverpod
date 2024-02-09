5831841601e8847a93b29fff958b166784ef2e81
degnonepithech <josue.allagbe@epitech.eu>
2024-02-09 14:37:57 +0100

    Async

--- lib/async.dart
+++ lib/async.dart
@@ -12,18 +12,16 @@ class NotifierAsync extends AsyncNotifier<int> {
 
   @override
   FutureOr<int> build() async {
-    // Initialisation asynchrone si nécessaire
-    // Par exemple, vous pourriez attendre une opération de chargement de données
-    // return await _loadData();
+    
     await Future.delayed(const Duration(seconds: 45));
     const Loading();
    _myNotifier2;
 
-    // Sinon, vous pouvez simplement retourner une valeur initiale
+    
     return 0;
   }
 
-  // Ajoutez vos méthodes pour interagir avec les notifiants
+  
 }
 
 // final asynProvider = AsyncNotifierProvider<NotifierAsync, int>(NotifierAsync.new);
