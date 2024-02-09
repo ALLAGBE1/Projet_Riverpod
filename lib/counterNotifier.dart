e37e48ae3a0c838de3c0e830dea12d64f4da9e9f
degnonepithech <josue.allagbe@epitech.eu>
2024-02-09 11:34:50 +0100

    Notifier

--- lib/counterNotifier.dart
+++ lib/counterNotifier.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyNotifier extends StateNotifier<int> {
-  MyNotifier() : super(0);
+class MyNotifier extends Notifier<int> {
+  @override
+  int build() {
+    return 0;
+  }
 
   void increment() {
     state++;
   }
 }
 
-final counterNotifierProvider = StateNotifierProvider<MyNotifier, int>((ref) {
-  return MyNotifier();
-});
+final counterNotifierProvider = NotifierProvider<MyNotifier, int>(MyNotifier.new);
