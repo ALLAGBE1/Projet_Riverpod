bd1af9ee9f605675ee4953d870f20d26382e5d46
degnonepithech <josue.allagbe@epitech.eu>
2024-02-09 15:04:04 +0100

    Async

--- lib/async.dart
+++ lib/async.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:projet_riverpod/counterNotifier.dart';
-import 'package:projet_riverpod/loading.dart';
 
 class NotifierAsync extends AsyncNotifier<int> {
   final MyNotifier _myNotifier;
@@ -11,17 +10,18 @@ class NotifierAsync extends AsyncNotifier<int> {
   NotifierAsync(this._myNotifier, this._myNotifier2);
 
   @override
-  FutureOr<int> build() async {
-    
-    await Future.delayed(const Duration(seconds: 45));
-    const Loading();
-   _myNotifier2;
-
-    
+  FutureOr<int> build() {
     return 0;
   }
 
-  
+  Future<int> addition(myNotifier, myNotifier2) async {
+    await Future.delayed(const Duration(seconds: 45));
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() async {
+      int add = myNotifier + myNotifier2;
+      return add;
+    });
+  }
 }
 
 // final asynProvider = AsyncNotifierProvider<NotifierAsync, int>(NotifierAsync.new);
